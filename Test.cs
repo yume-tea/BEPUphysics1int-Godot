e8b7578508b5e9a6e3dfbb1e6a37e02fa09d7363
yume tea <infernosnake@msn.com>
2024-01-09 23:02:11 -0800

    made sure physics update happens in the _PhysicsProcess()

--- Test.cs
+++ Test.cs
@@ -27,7 +27,7 @@ public partial class Test : Node
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public override void _PhysicsProcess(double delta)
 	{
 		// Update the space
 		space.Update();
